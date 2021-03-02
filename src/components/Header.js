c9c0a46deb3813aade4730f1129456c09ab15f1c
PallaviKadwe <kadwepallavi@johndeere.com>
2021-03-02 13:51:31 -0600

    Added adaptive display

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,29 +5,25 @@ import { Switch, Route, Link, Redirect } from 'react-router-dom';
 function Header(props) {
   console.log(props)
    return (
-       <nav className="box">
-           <div><h1>Pour Decisions</h1> </div>
-         
-         <div className = "linkStyle">
-         <div>
-             <Link to={'/'}> Home</Link></div>
+    <nav className="box">
+        <div><h1>Pour Decisions</h1> </div>
 
-             <form onSubmit={(event)=>{
+        <div className = "linkStyle">
+            <div>
+                <Link to={'/'}> Home</Link>
+            </div>
+
+            <form onSubmit={(event)=>{
                     event.preventDefault();
                     props.getDrinksByIngredient(event.target.drinkIngredient.value)
                     props.history.push('/Gallery/Search') /// allows for redirection to the Gallery page upon the Search condition being met in the Gallery if/else statement
                 }}>
-                    <input type="text" id="" name="drinkIngredient" />
+                    <input type="text" id="" name="drinkIngredient" placeholder="Ingredient to search"/> &nbsp;
                     <input type="submit" value="Submit" />
                     
-                </form>
-
-
-        {/* <form>
-         <input type = "text" placeholder = "ingredient"/>
-         </form> */}
-         </div>     
-       </nav>
+            </form>
+        </div>    
+    </nav>
    )
 }
 
